0f96dd6b72798b1bd93baee8e1a7b81f95531bd2
elsa-k-donovan <36769721+elsa-k-donovan@users.noreply.github.com>
2020-10-08 12:45:50 -0400

    Add files via upload

---
+++ twitter_cedar_dup.py
@@ -0,0 +1,82 @@
+import zipfile
+import os
+import csv
+import pandas as pd
+import shutil
+import glob
+import ntpath
+
+###########################################################################
+# Change all of the paths and variables in this section
+# for batch processing on Cedar.
+
+#root_path = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter"
+
+# path to zip file
+#path_zip = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/twitter_fixed.zip"
+path_zip = "/Volumes/Elsa_HD2/Memes/fb_test.zip"
+
+#path_zip_dest = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/Results"
+path_zip_dest = "/Volumes/Elsa_HD2/Memes/Twitter/Results"
+#path_zip_dir = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/twitter_fixed/"
+
+# path to csv
+#path_csv = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/twitter_filenames.csv"
+
+path_csv = "/Volumes/Elsa_HD2/Memes/Twitter/twitter_filenames.csv"
+
+zip_name = "fb_test"
+max_imgs_per_folder = 1
+
+###########################################################################
+
+# file formats that we expects to see in zip
+FileFormats = ['.jpg', '.png', '.gif']
+
+counter = 0
+files_moved_count = 0
+folder_num = 1
+
+# open csv file
+df = pd.read_csv(path_csv)
+
+name_col = df.filename
+cols = pd.Series(name_col)
+
+with zipfile.ZipFile(path_zip, 'r') as zip_ref:
+
+   # create first folder
+    dest_dir_path = path_zip_dest + "/folder_" + str(folder_num)
+
+    if not os.path.exists(dest_dir_path):
+        print("Creating new folder at " + dest_dir_path)
+        files_moved_count = 0
+        os.makedirs(dest_dir_path)
+
+    #iterate through cols.values
+    for file in cols.values:
+        #filename_path = path_zip_dir + file
+        #print(filename_path)
+
+        try:
+            full_file = str(zip_name) + "/" + str(file)
+            zip_ref.read(full_file)
+            if files_moved_count < max_imgs_per_folder:
+                try:
+                    zip_ref.extract(full_file, dest_dir_path)
+                except Exception as ex1:
+                    print("File extraction process did not work.")
+            else:
+                folder_num += 1
+                dest_dir_path = path_zip_dest + "/folder_" + str(folder_num)
+                if not os.path.exists(dest_dir_path):
+                    print("Creating new folder at " + dest_dir_path)
+                    files_moved_count = 0
+                    os.makedirs(dest_dir_path)
+                try:
+                    zip_ref.extract(full_file, dest_dir_path)
+                except Exception as ex1:
+                    print("File extraction process did not work.")
+            files_moved_count += 1
+        except Exception as ex2:
+            continue
