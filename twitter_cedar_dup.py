c1669c302e98df4936c254d39a99647193d796d1
elsa-k-donovan <36769721+elsa-k-donovan@users.noreply.github.com>
2020-10-13 12:04:44 -0400

    Update twitter_cedar_dup.py

--- twitter_cedar_dup.py
+++ twitter_cedar_dup.py
@@ -1,29 +1,30 @@
 import zipfile
 import os
-import csv
 import pandas as pd
 import shutil
-import glob
-import ntpath
+
 
 ###########################################################################
 # Change all of the paths and variables in this section
 # for batch processing on Cedar.
 
-#root_path = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter"
+root = "/Volumes/Elsa_HD2/Memes/"
 
 # path to zip file
-path_zip = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/twitter_fixed.zip"
+#path_zip = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/twitter_fixed.zip"
+path_zip = "/Volumes/Elsa_HD2/Memes/fb_test.zip"
 
-path_zip_dest = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/Results"
-
+#path_zip_dest = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/Results"
+path_zip_dest = "/Volumes/Elsa_HD2/Memes/Twitter/Results/"
 #path_zip_dir = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/twitter_fixed/"
 
 # path to csv
-path_csv = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/twitter_filenames.csv"
+#path_csv = "/home/edonovan/projects/def-whkchun/memes/images/CA_2019Elections/twitter/twitter_filenames.csv"
 
-zip_name = "twitter_fixed"
-max_imgs_per_folder = 100000
+path_csv = "/Volumes/Elsa_HD2/Memes/Twitter/twitter_filenames.csv"
+
+zip_name = "fb_test"
+max_imgs_per_folder = 1
 
 ###########################################################################
 
@@ -40,10 +41,21 @@ df = pd.read_csv(path_csv)
 name_col = df.filename
 cols = pd.Series(name_col)
 
+
+def make_archive(source, destination):
+    base = os.path.basename(destination)
+    name = base.split('.')[0]
+    format = base.split('.')[1]
+    archive_from = os.path.dirname(source)
+    archive_to = os.path.basename(source.strip(os.sep))
+    shutil.make_archive(name, format, archive_from, archive_to)
+    shutil.move('%s.%s' % (name, format), destination)
+
+
 with zipfile.ZipFile(path_zip, 'r') as zip_ref:
 
    # create first folder
-    dest_dir_path = path_zip_dest + "/folder_" + str(folder_num)
+    dest_dir_path = path_zip_dest + "folder_" + str(folder_num)
 
     if not os.path.exists(dest_dir_path):
         print("Creating new folder at " + dest_dir_path)
@@ -52,25 +64,19 @@ with zipfile.ZipFile(path_zip, 'r') as zip_ref:
 
     #iterate through cols.values
     for file in cols.values:
-
         try:
             full_file = str(zip_name) + "/" + str(file)
-            
-            # ISSUE on CEDAR begins here. The error message is always: line 10: /home/edonovan : is a directory
-            # It can't process the zip_ref.read(full_file).
-            # I have tried to change the full_file to just be the file name and not the 'str(zip_name) + "/"' part before.
-            # Nothing has worked so far. 
-            # It's able to iterate through the entire CSV successfully. Just the ZipFile commands don't work.
             zip_ref.read(full_file)
-            
+
             if files_moved_count < max_imgs_per_folder:
                 try:
+                    print("before extract")
                     zip_ref.extract(full_file, dest_dir_path)
                 except Exception as ex1:
                     print("File extraction process did not work.")
             else:
                 folder_num += 1
-                dest_dir_path = path_zip_dest + "/folder_" + str(folder_num)
+                dest_dir_path = path_zip_dest + "folder_" + str(folder_num)
                 if not os.path.exists(dest_dir_path):
                     print("Creating new folder at " + dest_dir_path)
                     files_moved_count = 0
@@ -80,5 +86,16 @@ with zipfile.ZipFile(path_zip, 'r') as zip_ref:
                 except Exception as ex1:
                     print("File extraction process did not work.")
             files_moved_count += 1
+            print(files_moved_count)
+
+            if files_moved_count >= max_imgs_per_folder:
+                new_path_dest = path_zip_dest + "zip_folder" + str(folder_num) + ".zip"
+                #it is zipping the Results folder instad of specific folder num
+                print(dest_dir_path)
+                try:
+                    make_archive(dest_dir_path, new_path_dest)
+                except Exception as ex0:
+                    print("zip file didn't work")
+                shutil.rmtree(dest_dir_path)
         except Exception as ex2:
             continue
